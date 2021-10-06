89ae06ad375e3484b9948f8df007d539f9382a10
Matija Popijač <matija.popijac@hotmail.com>
2021-10-06 17:38:09 +0200

    Initial commit

---
+++ src/main/java/hr/mpopijac/electricity_tracker/ElectricityTrackerApplication.java
@@ -0,0 +1,13 @@
+package hr.mpopijac.electricity_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/mpopijac/electricity_tracker/ElectricityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.mpopijac.electricity_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
